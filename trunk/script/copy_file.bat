b2d9ce7a395d425ecd7d517da4d60cd6ea87478e
wenjie.zhao <740936897@qq.com>
2015-02-04 02:31:39 +0800

    refine script.bat

--- trunk/script/copy_file.bat
+++ trunk/script/copy_file.bat
@@ -1,6 +1,5 @@
 xcopy /s /e /i /y 3rdparty\libopencv\runtime_dll bin\
 xcopy /s /e /i /y  src\stylesheet bin\stylesheet
 xcopy /s /e /i /y  src\theme bin\theme
-xcopy /s /e /i /y  src\font bin\font
 xcopy /q /y  src\res.ini bin\
 xcopy /q /y  src\setting.ini bin\
